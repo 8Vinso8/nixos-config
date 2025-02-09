a2582ef817a0ee3ec869cb9821602d98714b7c0f
Vinso <8vinso8@gmail.com>
2025-02-09 14:20:28 +1000

    refactor

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.vinso = import ./home.nix;
+            home-manager.users.vinso = import ./users/vinso/home.nix;
             home-manager.extraSpecialArgs = { inherit inputs; };
           }
         ];
