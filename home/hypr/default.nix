12355b44a5ca3b82365201fc27ea3ec52c5452eb
Vinso <8vinso8@gmail.com>
2025-02-10 11:45:02 +1000

    move all hypr to nix

--- home/hypr/default.nix
+++ home/hypr/default.nix
@@ -20,7 +20,6 @@
   wayland.windowManager.hyprland = {
     enable = true;
     systemd.enable = false;
-    extraConfig = builtins.readFile ./configs/hyprland.conf;
     settings = { 
       monitor = [
         "DP-1, 2560x1440@165, 0x0, 1"
@@ -29,8 +28,6 @@
       exec-once = [
         "uwsm app -- waybar"
       ];
-      "$terminal" = "kitty";
-      "$fileManager" = "yy";
       "$menu" = "fuzzel --launch-prefix='uwsm app -- '";
       "$mainMod" = "SUPER";
       general = {
@@ -70,7 +67,13 @@
         vrr = 2;
         key_press_enables_dpms = true;
       };
+      master = {
+        mfact = 0.70;
+        orientation = "right";
+
+      };
       workspace = [
+        # no gaps when only
         "w[tv1], gapsout:0, gapsin:0"
         "f[1], gapsout:0, gapsin:0"
       ];
@@ -85,6 +88,39 @@
         "rounding 0, floating:0, onworkspace:w[tv1]"
         "bordersize 0, floating:0, onworkspace:f[1]"
         "rounding 0, floating:0, onworkspace:f[1]"
+      ];
+      bind = [
+        "$mainMod, RETURN, exec, uwsm app -- kitty"
+        "$mainMod, C, killactive,"
+        "$mainMod, V, togglefloating"
+        "$mainMod, D, exec, $menu"
+
+        "$mainMod, left, movefocus, l"
+        "$mainMod, right, movefocus, r"
+        "$mainMod, up, movefocus, u"
+        "$mainMod, down, movefocus, d"
+
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, Q, workspace, 5"
+        "$mainMod, W, workspace, 6"
+        "$mainMod, E, workspace, 7"
+        "$mainMod, R, workspace, 8"
+        
+        "$mainMod SHIFT, 1, movetoworkspace, 1"
+        "$mainMod SHIFT, 2, movetoworkspace, 2"
+        "$mainMod SHIFT, 3, movetoworkspace, 3"
+        "$mainMod SHIFT, 4, movetoworkspace, 4"
+        "$mainMod SHIFT, Q, movetoworkspace, 5"
+        "$mainMod SHIFT, W, movetoworkspace, 6"
+        "$mainMod SHIFT, E, movetoworkspace, 7"
+        "$mainMod SHIFT, R, movetoworkspace, 8"
+      ];
+      bindm = [
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
       ];
     };
   };
