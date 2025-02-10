3a965b7de4a6dfeed7e7f4ddac4d8032ddf309da
Vinso <8vinso8@gmail.com>
2025-02-10 18:18:03 +1000

    waybar date

--- home/hypr/waybar.nix
+++ home/hypr/waybar.nix
@@ -13,7 +13,29 @@
         position = "top";
         modules-left = [ "hyprland/workspaces" ];
         modules-center = [ "hyprland/window" ];
-        modules-right = [ "cpu" "clock" "disk" "hyprland/language" "memory" "tray" ];
+        modules-right = [
+          "cpu"
+          "clock"
+          "disk"
+          "hyprland/language"
+          "memory"
+          "tray"
+        ];
+        "hyprland/workspaces" = {
+          persistent-workspaces = {
+            "*" = 8;
+          };
+        };
+        "hyprland-language" = {
+          "format-en" = "EN";
+          "format-ru" = "RU";
+        };
+        cpu = {
+          format = "CPU:{usage}%";
+        };
+        clock = {
+          format = "{:%Y.%m.%d %H:%M}";
+        };
       };
     };
   };
