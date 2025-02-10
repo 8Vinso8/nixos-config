1166e041b81c366ed7cfd966ddbb795a1d803e7a
Vinso <8vinso8@gmail.com>
2025-02-10 10:12:24 +1000

    yazi dependencies

--- home/programs/yazi.nix
+++ home/programs/yazi.nix
@@ -1,8 +1,21 @@
 {
-...
+  pkgs,
+  ...
 }:
 
 {
+  home.packages = with pkgs; [
+    ffmpeg # for video thumbnails
+    p7zip # for archive extraction and preview
+    jq # for json preview
+    poppler # for pdf preview
+    fd # for file searching
+    ripgrep # for file contents searching
+    fzf # for quick file subtree navigation
+    zoxide # for historical directories navigation
+    imagemagick # for SVG, Font, HEIC, and JPEG XL preview
+    wl-clipboard
+  ];
   programs.yazi = {
     enable = true;
     settings = {
