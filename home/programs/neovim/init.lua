3552eda6206eb5286ee04f1f03419d833e2ec68f
Vinso <8vinso8@gmail.com>
2025-02-14 14:33:12 +1000

    nvim coc formatting

--- home/programs/neovim/init.lua
+++ home/programs/neovim/init.lua
@@ -4,4 +4,4 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.number = true;
 
-vim.keymap.set("n", "ff", "call CocAction('format')", { remap = false })
+vim.keymap.set("n", "ff", "<CMD>call CocAction('format')<CR>", { noremap = true, silent = true })
