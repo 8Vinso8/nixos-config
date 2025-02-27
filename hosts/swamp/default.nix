a3d7d0d2db6785f87a2ebbf8a68d89e6f8c3aa50
Vinso <8vinso8@gmail.com>
2025-02-27 11:54:02 +1000

    new host init

---
+++ hosts/swamp/default.nix
@@ -0,0 +1,19 @@
+{
+  pkgs,
+  ...
+}:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules/default.nix
+    ../../modules/sdboot
+    ../../modules/silentBoot.nix
+    ../../modules/sing-box.nix
+    ../../modules/features/bluetooth
+    ../../modules/services/pipewire.nix
+  ];
+  boot.kernelPackages = pkgs.linuxPackages_zen;
+  networking.hostName = "swamp";
+  system.stateVersion = "24.11";
+}
