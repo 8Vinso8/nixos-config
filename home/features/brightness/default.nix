7c058c6a6c681240d053b7cc58137ca2abac784e
Vinso <8vinso8@gmail.com>
2025-02-28 11:17:17 +1000

    remove title from osd messages

--- home/features/brightness/default.nix
+++ home/features/brightness/default.nix
@@ -12,8 +12,8 @@
   wayland.windowManager.hyprland = {
     settings = {
       bind = [
-        "$mainMod, bracketright, exec, ddcutil --noverify --sleep-multiplier 0.1 --skip-ddc-checks setvcp 10 + 10 && notify-send -h string:x-canonical-private-synchronous:brightness Brightness -h int:value:\"$(ddcutil --terse --sleep-multiplier 0.1 getvcp 10 | grep -oP '^(?:\\S+\\s+){3}\\K\\d+')\""
-        "$mainMod, bracketleft, exec, ddcutil --noverify --sleep-multiplier 0.1 --skip-ddc-checks setvcp 10 - 10 && notify-send -h string:x-canonical-private-synchronous:brightness Brightness -h int:value:\"$(ddcutil --terse --sleep-multiplier 0.1 getvcp 10 | grep -oP '^(?:\\S+\\s+){3}\\K\\d+')\""
+        "$mainMod, bracketright, exec, ddcutil --noverify --sleep-multiplier 0.1 --skip-ddc-checks setvcp 10 + 10 && notify-send -h string:x-canonical-private-synchronous:brightness \"\" -h int:value:\"$(ddcutil --terse --sleep-multiplier 0.1 getvcp 10 | grep -oP '^(?:\\S+\\s+){3}\\K\\d+')\""
+        "$mainMod, bracketleft, exec, ddcutil --noverify --sleep-multiplier 0.1 --skip-ddc-checks setvcp 10 - 10 && notify-send -h string:x-canonical-private-synchronous:brightness \"\" -h int:value:\"$(ddcutil --terse --sleep-multiplier 0.1 getvcp 10 | grep -oP '^(?:\\S+\\s+){3}\\K\\d+')\""
       ];
     };
   };
