9c3821062a36ce0f0e684a204ded058c70dcc0e6
Vinso <8vinso8@gmail.com>
2025-03-03 11:26:24 +1000

    keybinds for hypr i2c

--- home/features/brightness/ddcutil.nix
+++ home/features/brightness/ddcutil.nix
@@ -1,20 +1,12 @@
 {
-  pkgs,
   ...
 }:
 
 {
-  # WARNING: Requires hardware.i2c.enable = true
-  home.packages = [
-    pkgs.ddcutil
-  ];
-
   wayland.windowManager.hyprland = {
-    settings = {
-      bind = [
-        "$mainMod, bracketright, exec, ddcutil --noverify --sleep-multiplier 0.1 --skip-ddc-checks setvcp 10 + 10 && notify-send -h string:x-canonical-private-synchronous:brightness ' ' \"​\" -h int:value:\"$(ddcutil --terse --sleep-multiplier 0.1 getvcp 10 | grep -oP '^(?:\\S+\\s+){3}\\K\\d+')\""
-        "$mainMod, bracketleft, exec, ddcutil --noverify --sleep-multiplier 0.1 --skip-ddc-checks setvcp 10 - 10 && notify-send -h string:x-canonical-private-synchronous:brightness ' ' \"​\" -h int:value:$(ddcutil --terse --sleep-multiplier 0.1 getvcp 10 | grep -oP '^(?:\\S+\\s+){3}\\K\\d+')"
-      ];
-    };
+    extraConfig = ''
+      bind = $mainMod, bracketright, exec, for bus in $(cat /run/monitor_i2c_buses); do ddcutil -b $bus setvcp 10 + 10; notify-send -h string:x-canonical-private-synchronous:brightness ' ' -h int:value:$(ddcutil -b $bus getvcp 10 | grep -oP 'current value =\s+\K\d+'); done
+      bind = $mainMod, bracketleft, exec, for bus in $(cat /run/monitor_i2c_buses); do ddcutil -b $bus setvcp 10 - 10; notify-send -h string:x-canonical-private-synchronous:brightness ' ' -h int:value:$(ddcutil -b $bus getvcp 10 | grep -oP 'current value =\s+\K\d+'); done
+    '';
   };
 }
--- modules/features/brightness/default.nix
+++ modules/features/brightness/default.nix
@@ -5,17 +5,24 @@
 
 {
   hardware.i2c.enable = true;
-  environment.systemPackages = [ pkgs.ddcutil ]; 
-  systemd.services.detect-i2c = {
-    description = "Detect Monitor I2C Buses";
-    after = [ "network.target" ];
+  environment.systemPackages = [ pkgs.ddcutil ];
+  systemd.services.detect-i2c-buses = {
+    description = "Detect monitor I2C buses";
     wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
     serviceConfig = {
       Type = "oneshot";
       RemainAfterExit = true;
-      ExecStart = pkgs.bash + "/bin/bash -c '\
-        export I2C_BUSES=\"$(ddcutil detect | awk \"/I2C bus:/ {print \\$3}\" | tr \"\\n\" \" \")\" && \
-        systemctl set-environment I2C_BUSES=\"$I2C_BUSES\"'";
+      ExecStart =
+        let
+          detectScript = pkgs.writeShellScript "detect-i2c-buses" ''
+            set -e
+            buses=$(${pkgs.ddcutil}/bin/ddcutil --disable-dynamic-sleep detect --brief | grep -oP '/dev/i2c-\K\d+' | tr '\n' ' ')
+            echo "''${buses%% }" > /run/monitor_i2c_buses
+            chmod 644 /run/monitor_i2c_buses
+          '';
+        in
+        "${detectScript}";
     };
   };
 }
