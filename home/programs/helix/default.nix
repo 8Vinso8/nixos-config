e0a4a9845b04b084c567743abcd9acd4b6bbf1f5
Vinso <8vinso8@gmail.com>
2025-03-09 20:40:46 +1000

    helix theme

--- home/programs/helix/default.nix
+++ home/programs/helix/default.nix
@@ -7,8 +7,42 @@
   programs.helix = {
     enable = true;
     extraPackages = with pkgs; [
-      python313Packages.python-lsp-server
-      nil
+      nixd
+      nixfmt-rfc-style
+      ruff
+      basedpyright
+      vscode-langservers-extracted
     ];
+    settings = {
+      theme = "my_theme";
+    };
+    themes = {
+      my_theme = {
+        inherits = "catppuccin_mocha";
+        "ui.background" = "none";
+      };
+    };
+    languages = {
+      language = [
+        {
+          name = "nix";
+          language-servers = [ "nixd" ];
+          formatter = {
+            command = "nixfmt";
+          };
+        }
+        {
+          name = "python";
+          language-servers = [ "basedpyright" "ruff" ];
+
+        }
+      ];
+      language-server = {
+        ruff = {
+          command = "ruff";
+          args = [ "server" ];
+        };
+      };
+    };
   };
 }
