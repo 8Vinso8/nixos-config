188cdb57d3eba551113e38ef958886f94e6834f6
Vinso <8vinso8@gmail.com>
2025-06-14 21:01:55 +1000

    nvim cmp

--- home.nix
+++ home.nix
@@ -1,14 +1,16 @@
-{ config, pkgs, inputs, ...}:
+{
+  pkgs,
+  inputs,
+  ...
+}:
 
 {
   imports = [
     inputs.nixvim.homeModules.nixvim
   ];
-
-
   home.username = "vinso";
   home.homeDirectory = "/home/vinso";
-  home.packages = with pkgs; [];
+  home.packages = with pkgs; [ ];
   programs.git = {
     enable = true;
     userName = "Vinso";
@@ -41,9 +43,50 @@
       };
       virtual_text = false;
     };
+    opts = {
+      number = true;
+      expandtab = true;
+      cursorline = true;
+      smartcase = true;
+      tabstop = 2;
+      softtabstop = 2;
+      shiftwidth = 2;
+    };
+    extraPackages = with pkgs; [
+      nixfmt-rfc-style
+    ];
     plugins.lspconfig.enable = true;
+    plugins.indent-blankline.enable = true;
+    plugins.cmp = {
+      enable = true;
+      settings.sources = [
+        { name = "nvim_lsp"; }
+        { name = "vsnip"; }
+        { name = "path"; }
+        { name = "buffer"; }
+      ];
+      settings.mapping = {
+        "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select}))";
+        "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select}))";
+        "<CR>" = "cmp.mapping.confirm({ select = true })";
+      };
+      settings.preselect = "cmp.PreselectMode.None";
+    };
     lsp.inlayHints.enable = true;
     lsp.servers.nixd.enable = true;
+    lsp.servers.nixd.settings = {
+      formatting.command = [ "nixfmt" ];
+    };
+    lsp.keymaps = [
+      {
+        key = "ff";
+        lspBufAction = "format";
+      }
+      {
+        key = "ca";
+        lspBufAction = "code_action";
+      }
+    ];
   };
   home.stateVersion = "25.05";
 }
