d80f29e4e52815d7cf647ab3d6acf6f19b18d2ec
Vinso <8vinso8@gmail.com>
2025-06-14 10:46:58 +1000

    home manager

--- flake.nix
+++ flake.nix
@@ -5,15 +5,26 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     secrets.url = "git+ssh://git@github.com/8Vinso8/secrets.git";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations = {
       nixvm = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
 	specialArgs = { inherit inputs; };
 	modules = [
           ./configuration.nix
+	  home-manager.nixosModules.home-manager
+	  {
+            home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+	    home-manager.extraSpecialArgs = { inherit inputs; };
+	    home-manager.users.vinso = import ./home.nix;
+	  }
 	];
       };
     };
---
+++ home.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ...}:
+
+{
+  home.username = "vinso";
+  home.homeDirectory = "/home/vinso";
+  home.packages = with pkgs; [];
+  programs.git = {
+    enable = true;
+    userName = "Vinso";
+    userEmail = "8vinso8@gmail.com";
+  };
+  home.stateVersion = "25.05";
+}
