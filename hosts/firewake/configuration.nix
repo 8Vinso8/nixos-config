dbc13bfa571958d9f87dc8cd3c3e0c22f33913e6
Vinso <8vinso8@gmail.com>
2026-09-17 22:18:10 +1000

    cache remove
    cache is on hydra now, no need to manually add

--- hosts/firewake/configuration.nix
+++ hosts/firewake/configuration.nix
@@ -8,9 +8,6 @@
 
   nixpkgs.overlays = [ inputs.nix-cachyos-kernel.overlays.pinned ];
   boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest-x86_64-v3;
-  # Binary cache fof cachy kernel
-  nix.settings.substituters = [ "https://attic.xuyh0120.win/lantian" ];
-  nix.settings.trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
 
   boot.initrd.verbose = false;
   boot.initrd.systemd.enable = true;
